903ab804fba4d7f890d0fae9d5fcb40d451cfcee
yabumoto-takahito <yammo1107@yahoo.co.jp>
2019-09-29 23:19:29 +0900

    Initial Commit

---
+++ StarTrivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarTrivia
+//
+//  Created by 薮本崇仁 on 2019/09/29.
+//  Copyright © 2019 薮本崇仁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
