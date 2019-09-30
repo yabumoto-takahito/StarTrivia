00d461aa469d281cbb8c62723874f5ec5fc7a2dd
yabumoto-takahito <yammo1107@yahoo.co.jp>
2019-10-01 00:03:00 +0900

    Creating the Main View
    Main Viewの作成。

--- StarTrivia/SelectPersonVC.swift
+++ StarTrivia/SelectPersonVC.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class SelectPersonVC: UIViewController {
 
+    @IBOutlet weak var stackBg: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        stackBg.layer.cornerRadius = 10
     }
 
 
