0ecb0f8994116b5c99934748424eaa02e35d361d
yabumoto-takahito <yammo1107@yahoo.co.jp>
2019-10-23 00:40:25 +0900

    Setting up additional VCs

---
+++ StarTrivia/Controller/FilmsVC.swift
@@ -0,0 +1,30 @@
+//
+//  FilmsVC.swift
+//  StarTrivia
+//
+//  Created by 薮本崇仁 on 2019/10/23.
+//  Copyright © 2019 薮本崇仁. All rights reserved.
+//
+
+import UIKit
+
+class FilmsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
